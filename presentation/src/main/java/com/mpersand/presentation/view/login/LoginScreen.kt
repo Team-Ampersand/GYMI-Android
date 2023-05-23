72575aefb2a5cc1fcf398d50987959da67d0235d
hyeonseo <s21035@gsm.hs.kr>
2023-05-23 11:34:41 +0900

    :lipstick: :: Spacer height값 변경 (112.dp -> 110.dp)

--- presentation/src/main/java/com/mpersand/presentation/view/login/LoginScreen.kt
+++ presentation/src/main/java/com/mpersand/presentation/view/login/LoginScreen.kt
@@ -1,6 +1,7 @@
 package com.mpersand.presentation.view.login
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,10 +10,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mpersand.presentation.R
@@ -46,6 +49,6 @@ fun LoginScreen(modifier: Modifier = Modifier) {
         ) {
 
         }
-        Spacer(modifier = modifier.height(112.dp))
+        Spacer(modifier = modifier.height(110.dp))
     }
 }
