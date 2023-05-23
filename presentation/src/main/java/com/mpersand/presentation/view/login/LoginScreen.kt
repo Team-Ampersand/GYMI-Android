d66f5e460b18900321f663d28b07d286ae659b6e
hyeonseo <s21035@gsm.hs.kr>
2023-05-23 11:38:10 +0900

    :fire: :: 불필요한 import 삭제

--- presentation/src/main/java/com/mpersand/presentation/view/login/LoginScreen.kt
+++ presentation/src/main/java/com/mpersand/presentation/view/login/LoginScreen.kt
@@ -1,7 +1,6 @@
 package com.mpersand.presentation.view.login
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,12 +9,10 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mpersand.presentation.R
