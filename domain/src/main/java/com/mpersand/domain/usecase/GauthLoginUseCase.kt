74fff0d32afe2d0f3c3ce4c367c9db1b85200a3b
박성현 <pjhomg5530@naver.com>
2023-07-26 18:19:21 +0900

    :sparkles: :: usecase 생성

---
+++ domain/src/main/java/com/mpersand/domain/usecase/GauthLoginUseCase.kt
@@ -0,0 +1,11 @@
+package com.mpersand.domain.usecase
+
+import com.mpersand.domain.model.auth.request.GauthLoginRequestData
+import com.mpersand.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class GauthLoginUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke(gAuthLoginRequestData: GauthLoginRequestData) = authRepository.gAuthLogin(gAuthLoginRequestData)
+}
