d13781e77badae8446641bd215a8af1e7cd6ef8d
박성현 <pjhomg5530@naver.com>
2023-07-26 18:18:55 +0900

    :sparkles: :: mapper 생성

--- data/src/main/java/com/mpersand/data/remote/model/auth/gAuthLogin/request/GauthLoginRequest.kt
+++ data/src/main/java/com/mpersand/data/remote/model/auth/gAuthLogin/request/GauthLoginRequest.kt
@@ -1,6 +1,13 @@
 package com.mpersand.data.remote.model.auth.gAuthLogin.request
 
+import com.mpersand.domain.model.auth.request.GauthLoginRequestData
+
 data class GauthLoginRequest(
     val code: String,
     val token: String?
+)
+
+fun GauthLoginRequestData.asGauthLoginRequest() = GauthLoginRequest(
+    code = code,
+    token = token
 )
--- data/src/main/java/com/mpersand/data/remote/model/auth/gAuthLogin/response/GauthLoginResponse.kt
+++ data/src/main/java/com/mpersand/data/remote/model/auth/gAuthLogin/response/GauthLoginResponse.kt
@@ -1,5 +1,6 @@
 package com.mpersand.data.remote.model.auth.gAuthLogin.response
 
+import com.mpersand.domain.model.auth.response.GauthLoginResponseData
 import java.time.ZonedDateTime
 
 data class GauthLoginResponse(
@@ -7,4 +8,11 @@ data class GauthLoginResponse(
     val refreshToken: String,
     val accessExp: ZonedDateTime,
     val refreshExp: ZonedDateTime
+)
+
+fun GauthLoginResponse.asGauthLoginResponseData() = GauthLoginResponseData(
+    accessToken = accessToken,
+    refreshToken = refreshToken,
+    accessExp = accessExp,
+    refreshExp = refreshExp
 )
