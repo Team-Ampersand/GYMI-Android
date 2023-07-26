f435958e314811f52c3340394d5795df001ab21b
박성현 <pjhomg5530@naver.com>
2023-07-26 17:25:46 +0900

    :recycle: :: 빼먹은 endpoint, @Body 붙이기

--- data/src/main/java/com/mpersand/data/remote/network/AuthApi.kt
+++ data/src/main/java/com/mpersand/data/remote/network/AuthApi.kt
@@ -3,17 +3,22 @@ package com.mpersand.data.remote.network
 import com.mpersand.data.remote.model.auth.gAuthLogin.request.GauthLoginRequest
 import com.mpersand.data.remote.model.auth.gAuthLogin.response.GauthLoginResponse
 import com.mpersand.data.remote.model.auth.tokenReissue.request.TokenReissueRequest
+import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface AuthApi {
-    @POST
-    suspend fun gAuthLogin(gAuthLoginRequest: GauthLoginRequest): GauthLoginResponse
+    @POST("auth")
+    suspend fun gAuthLogin(
+        @Body gAuthLoginRequest: GauthLoginRequest
+    ): GauthLoginResponse
 
-    @DELETE
+    @DELETE("auth")
     suspend fun gAuthLogout()
 
-    @PATCH
-    suspend fun tokenReissue(tokenReissueRequest: TokenReissueRequest): GauthLoginResponse
+    @PATCH("auth")
+    suspend fun tokenReissue(
+        @Body tokenReissueRequest: TokenReissueRequest
+    ): GauthLoginResponse
 }
