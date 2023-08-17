7c48bd40560dde9595b2f2fa0520847e67b34fe9
박성현 <pjhomg5530@naver.com>
2023-08-17 15:22:39 +0900

    :fire: :: preview 지우기

--- presentation/src/main/java/com/mpersand/presentation/view/main/MainScreen.kt
+++ presentation/src/main/java/com/mpersand/presentation/view/main/MainScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mpersand.gymi_components.component.court.GYMIBadmintonCourt
 import com.mpersand.gymi_components.component.court.GYMIBasketballCourt
@@ -128,15 +127,4 @@ fun MainScreen(
             }
         }
     }
-}
-
-@Preview
-@Composable
-fun preview() {
-    MainScreen(
-        dayOfWeek = "수",
-        navigateToMain = {},
-        navigateToNotice = {},
-        navigateToProfile = {}
-    )
 }
