a816b0d3396a3f415c328dec0f5d1d68a07a8d41
박성현 <pjhomg5530@naver.com>
2023-08-17 11:43:17 +0900

    :lipstick: :: MainScreen 퍼블리싱

---
+++ presentation/src/main/java/com/mpersand/presentation/view/main/MainScreen.kt
@@ -0,0 +1,77 @@
+package com.mpersand.presentation.view.main
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.LocalContentColor
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mpersand.gymi_components.component.court.GYMIBadmintonCourt
+import com.mpersand.gymi_components.component.header.GYMIHeader
+import com.mpersand.gymi_components.component.navbar.GYMINavBar
+import com.mpersand.gymi_components.component.navbar.GYMINavItem
+import com.mpersand.gymi_components.theme.GYMITheme
+import com.mpersand.gymi_components.theme.GYMITypography
+import com.mpersand.gymi_components.theme.IcEquipment
+import com.mpersand.gymi_components.theme.IcHome
+import com.mpersand.gymi_components.theme.IcReservation
+
+@Composable
+fun MainScreen(
+    modifier: Modifier = Modifier,
+    navigateToMain: () -> Unit,
+    navigateToNotice: () -> Unit,
+    navigateToProfile: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(GYMITheme.colors.bg)
+    ) {
+        GYMIHeader(
+            navigateToMain = navigateToMain,
+            navigateToNotice = navigateToNotice,
+            navigationToProfile = navigateToProfile
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        Column(modifier = modifier.padding(horizontal = 20.dp)) {
+            Text(
+                text = "코트 예약 현황",
+                style = GYMITypography.h4
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            repeat(4) {
+                GYMIBadmintonCourt(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    isReserved = false
+                ) {}
+                Spacer(modifier = Modifier.height(2.dp))
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+            GYMINavBar {
+                GYMINavItem(selected = false, icon = { IcReservation(tint = LocalContentColor.current) }) {}
+                GYMINavItem(selected = true, icon = { IcHome(tint = LocalContentColor.current) }) {}
+                GYMINavItem(selected = false, icon = { IcEquipment(tint = LocalContentColor.current) }) {}
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun preview() {
+    MainScreen(
+        navigateToMain = {},
+        navigateToNotice = {},
+        navigateToProfile = {}
+    )
+}
