096e9bb5114bba38909e0d1a08b2d035bb7bfdc1
hyeonseo <s21035@gsm.hs.kr>
2023-08-18 16:04:12 +0900

    :lipstick: :: 디자인 시스템 적용

--- presentation/src/main/java/com/mpersand/presentation/view/login/LoginScreen.kt
+++ presentation/src/main/java/com/mpersand/presentation/view/login/LoginScreen.kt
@@ -9,10 +9,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.mpersand.gymi_components.theme.GYMITheme
 import com.mpersand.gymi_components.theme.IcGYMILogo
 import com.msg.gauthsignin.component.GAuthButton
 import com.msg.gauthsignin.component.utils.Types
@@ -28,9 +27,9 @@ fun LoginScreen(modifier: Modifier = Modifier) {
         Spacer(modifier = modifier.height(50.dp))
         Text(
             text = "GYMI는 교내 체육관 서비스입니다\n에약을 통해 체육관을 이용해 보세요!",
-            fontSize = 18.sp,
-            fontWeight = FontWeight.Medium,
-            textAlign = TextAlign.Center
+            style = GYMITheme.typography.subtitle2,
+            textAlign = TextAlign.Center,
+            color = GYMITheme.colors.bw
         )
         Spacer(modifier = modifier.weight(1f))
         GAuthButton(
