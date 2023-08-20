a2cef0132a99f98ff54fa4abd671c4125bd228bc
박성현 <pjhomg5530@naver.com>
2023-08-20 17:56:46 +0900

    :fire: :: preview 지우기

--- presentation/src/main/java/com/mpersand/presentation/view/notice/list/NoticeListScreen.kt
+++ presentation/src/main/java/com/mpersand/presentation/view/notice/list/NoticeListScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mpersand.gymi_components.component.header.GYMIHeader
 import com.mpersand.gymi_components.component.item.GYMINoticeItem
@@ -92,11 +91,4 @@ fun NoticeListScreen(
             }
         }
     }
-
-}
-
-@Preview
-@Composable
-fun preview() {
-    NoticeListScreen()
 }
