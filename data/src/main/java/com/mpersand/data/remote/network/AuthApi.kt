84743b57bb5d76e8e0dda148963d68c126540ef9
hyeonseo <s21035@gsm.hs.kr>
2023-08-30 17:01:06 +0900

    :fire: :: 토큰 재발급 로직 삭제

--- data/src/main/java/com/mpersand/data/remote/network/AuthApi.kt
+++ data/src/main/java/com/mpersand/data/remote/network/AuthApi.kt
@@ -2,10 +2,8 @@ package com.mpersand.data.remote.network
 
 import com.mpersand.data.remote.model.auth.gAuthLogin.request.GauthLoginRequest
 import com.mpersand.data.remote.model.auth.gAuthLogin.response.GauthLoginResponse
-import com.mpersand.data.remote.model.auth.tokenReissue.request.TokenReissueRequest
 import retrofit2.http.Body
 import retrofit2.http.DELETE
-import retrofit2.http.PATCH
 import retrofit2.http.POST
 
 interface AuthApi {
@@ -16,9 +14,4 @@ interface AuthApi {
 
     @DELETE("auth")
     suspend fun gAuthLogout()
-
-    @PATCH("auth")
-    suspend fun tokenReissue(
-        @Body tokenReissueRequest: TokenReissueRequest
-    ): GauthLoginResponse
 }
