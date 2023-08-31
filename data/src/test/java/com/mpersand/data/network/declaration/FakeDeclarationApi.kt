c8f0d9e9bb1ae767a816294f179598a1e1fe6b7e
hyeonseo <s21035@gsm.hs.kr>
2023-08-31 12:19:43 +0900

    :sparkles: :: DeclarationDataSourceTest

---
+++ data/src/test/java/com/mpersand/data/network/declaration/FakeDeclarationApi.kt
@@ -0,0 +1,17 @@
+package com.mpersand.data.network.declaration
+
+import com.mpersand.data.remote.model.declaration.request.DeclarationRequest
+import com.mpersand.data.remote.model.declaration.response.DeclarationResponse
+import com.mpersand.data.remote.network.DeclarationApi
+
+class FakeDeclarationApi(private var declarations: List<DeclarationResponse>) : DeclarationApi {
+    override suspend fun getAllDeclarations(): List<DeclarationResponse> = declarations
+
+    override suspend fun getDeclarationById(id: Long): DeclarationResponse = declarations.single { it.id == id }
+
+    override suspend fun submitDeclaration(body: DeclarationRequest) {
+        declarations = declarations.plus(
+            DeclarationResponse(id = declarations.size.toLong(), type = body.type, content = body.content)
+        )
+    }
+}
