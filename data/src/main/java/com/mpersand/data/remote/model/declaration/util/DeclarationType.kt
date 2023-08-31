e5d36fdb23b2a3b2eed385eed0a9fb0dc87aa124
hyeonseo <s21035@gsm.hs.kr>
2023-08-31 11:02:01 +0900

    :fire: :: `@JsonClass(generateAdapter = true)` 삭제

--- data/src/main/java/com/mpersand/data/remote/model/declaration/util/DeclarationType.kt
+++ data/src/main/java/com/mpersand/data/remote/model/declaration/util/DeclarationType.kt
@@ -1,8 +1,5 @@
 package com.mpersand.data.remote.model.declaration.util
 
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
 enum class DeclarationType {
     TIME, EQUIPMENT, SEAT, UNIFORM, FOOD, Dissension, ETC
 }
