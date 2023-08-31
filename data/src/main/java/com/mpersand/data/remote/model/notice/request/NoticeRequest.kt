67003715ba72075fcfc3169b073294e3aeeb9aa9
박성현 <pjhomg5530@naver.com>
2023-08-31 14:01:08 +0900

    :fire: :: 안 쓰이는 request 삭제

--- data/src/main/java/com/mpersand/data/remote/model/notice/request/NoticeRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.mpersand.data.remote.model.notice.request
-
-import com.squareup.moshi.JsonClass
-
-@JsonClass(generateAdapter = true)
-data class NoticeRequest(
-    val body: NoticeBody
-)
