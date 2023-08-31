495f80c4ff9439e48d06d7644441687228e652c4
박성현 <pjhomg5530@naver.com>
2023-08-31 12:33:39 +0900

    :sparkles: :: Notice api 구현

---
+++ data/src/main/java/com/mpersand/data/remote/network/NoticeApi.kt
@@ -0,0 +1,37 @@
+package com.mpersand.data.remote.network
+
+import com.mpersand.data.remote.model.notice.request.NoticeRequest
+import com.mpersand.data.remote.model.notice.response.NoticeDetailResponse
+import com.mpersand.data.remote.model.notice.response.NoticeResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface NoticeApi {
+    @POST("notice")
+    suspend fun createNotice(
+        @Body noticeRequest: NoticeRequest
+    )
+
+    @DELETE("notice/{id}")
+    suspend fun deleteNotice(
+        @Path("id") id: Long
+    )
+
+    @PATCH("notice/{id}")
+    suspend fun modifyNotice(
+        @Path("id") id: Long,
+        @Body noticeRequest: NoticeRequest
+    )
+
+    @GET("notice")
+    suspend fun getAllNotice(): NoticeResponse
+
+    @GET("notice/{id}")
+    suspend fun getDetailNotice(
+        @Path("id") id: Long
+    ): NoticeDetailResponse
+}
