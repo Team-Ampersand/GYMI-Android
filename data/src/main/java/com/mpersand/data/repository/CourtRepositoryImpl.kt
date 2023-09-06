5f8015fd413bc8ece96f03870c0b450e5f1d7920
hyeonseo <s21035@gsm.hs.kr>
2023-09-06 12:27:44 +0900

    :sparkles: :: CourtRepositoryImpl

---
+++ data/src/main/java/com/mpersand/data/repository/CourtRepositoryImpl.kt
@@ -0,0 +1,19 @@
+package com.mpersand.data.repository
+
+import com.mpersand.data.remote.datasource.court.CourtDataSource
+import com.mpersand.data.remote.model.court.response.asCourtResponseModel
+import com.mpersand.domain.model.court.response.CourtResponseModel
+import com.mpersand.domain.repository.CourtRepository
+import javax.inject.Inject
+
+class CourtRepositoryImpl @Inject constructor(
+    private val courtDataSource: CourtDataSource
+) : CourtRepository {
+    override suspend fun banCourtById(courtId: Long) = courtDataSource.banCourtById(courtId)
+
+    override suspend fun getAllCourts(): List<CourtResponseModel> =
+        courtDataSource.getAllCourts().map { it.asCourtResponseModel() }
+
+    override suspend fun getCourtById(courtId: Long): CourtResponseModel =
+        courtDataSource.getCourtById(courtId).asCourtResponseModel()
+}
