8283ef5bceae3063cb868b448f5e24db6e0fb3c7
hyeonseo <s21035@gsm.hs.kr>
2023-09-25 18:07:05 +0900

    :sparkles: :: 코트 예약 취소 로직

--- presentation/src/main/java/com/mpersand/presentation/view/reservation/ReservationScreen.kt
+++ presentation/src/main/java/com/mpersand/presentation/view/reservation/ReservationScreen.kt
@@ -7,19 +7,51 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.mpersand.domain.model.reservation.request.CourtNumberModel
+import com.mpersand.gymi_components.component.dialog.GYMIDialog
 import com.mpersand.gymi_components.theme.GYMITheme
 import com.mpersand.presentation.util.DayOfWeekType
 import com.mpersand.presentation.util.getDayOfWeekType
 import com.mpersand.presentation.view.component.BadmintonHalfCourt
 import com.mpersand.presentation.view.component.BasketballHalfCourt
+import com.mpersand.presentation.view.component.CourtButtonType
+import com.mpersand.presentation.view.component.CourtModal
+import com.mpersand.presentation.viewmodel.reservation.ReservationViewModel
 
 @Composable
 fun ReservationScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    reservationViewModel: ReservationViewModel = hiltViewModel()
 ) {
+
+    var showDialog by remember { mutableStateOf(false) }
+    /* TODO: 예약 현황에 따라 처리 */
+    var reserved by remember { mutableStateOf<CourtNumberModel?>(null) }
+
+    if (showDialog) {
+        GYMIDialog(onDismissRequest = { showDialog = false }) {
+            CourtModal(
+                courtName = "",  /* TODO: 코트 상세 받아와서 표시 */
+                imageUrl = "",
+                personnel = "",  /* TODO: 코트 상세 받아와서 표시 */
+                description = "이미 예약된 코트입니다.\n규칙을 어겼다면 신고해주세요!",
+                buttonType = CourtButtonType.Cancel,
+                onButtonClick = {
+                    reserved?.let { reservationViewModel.cancelReservation(it) }
+                },
+                onDismissRequest = { showDialog = false }
+            )
+        }
+    }
+
     Column(
         modifier = modifier.padding(horizontal = 20.dp),
         verticalArrangement = Arrangement.spacedBy(5.dp)
